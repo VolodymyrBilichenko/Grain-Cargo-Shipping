823b77db14bba11c92deb899ea34041b84fa9bac
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-12-04 16:17:51 +0200

    update

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,15 @@
-const swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    navigation: {
-        nextEl: ".button-next",
-        prevEl: ".button-prev",
-    },
-});
+// header menu
+
+const headerMenu = document.querySelector('.header__menu__head');
+const headerMenuBody = document.querySelector('.header__menu__body');
+const headerMenuClose = document.querySelector('.header__close');
+
+headerMenu.addEventListener('click', () => {
+    headerMenu.classList.add('header__menu__head_active');
+    headerMenuBody.classList.add('header__menu__body_active');
+})
+
+headerMenuClose.addEventListener('click', () => {
+    headerMenu.classList.remove('header__menu__head_active');
+    headerMenuBody.classList.remove('header__menu__body_active');
+  });
